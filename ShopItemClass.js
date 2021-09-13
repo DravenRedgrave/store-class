a33ff829ef3ca71d31b3992f8c46b13d746ddf94
DravenRedgrave <77494310+DravenRedgrave@users.noreply.github.com>
2021-09-13 22:08:41 +0300

    Add files via upload

---
+++ ShopItemClass.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class ShopItemClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      brand: 'Tiger of Sweden',
+      title: 'Leonard coat',
+      description: 'Minimalistic coat in cotton-blend',
+      descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+      price: 399,
+      currency: '£'
+    }
+  }
+  render() {
+  return(
+    <div class="main-content">
+    <h2>{this.state.brand}</h2>
+    <h1>{this.state.title}</h1>
+    <h3>{this.state.description}</h3>
+    <div class="description">
+      {this.state.descriptionFull}
+    </div>
+    <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+    <div class="divider"></div>
+    <div class="purchase-info">
+      <div class="price"> {this.state.currency}{this.state.price}.00</div>
+      <button>Добавить в корзину</button>
+    </div>
+  </div>
+  )
+  }
+}
+
+export default ShopItemClass;
